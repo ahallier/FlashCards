57144d77d53cae83d8b5d005d4f4915f94c961ec
Sydney Garcia <sydney-garcia@uiowa.edu>
2016-11-17 16:26:28 +0000

    trying agian

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -46,7 +46,8 @@ class DecksController < ApplicationController
         # the call to lower() make the search case insensitive
         ordering = 'lower('+sort+') '+ asc_or_desc
 
-        @decks = Deck.search(params[:search]).where(public: true).order(ordering).paginate(:per_page => 5, :page => params[:page])
+        @decks = Deck.search(params[:search]).where(public: true).order(ordering)
+        @decks = @decks.paginate(:per_page => 5, :page => params[:page])
         
         # the random token is used to ensure that the ordering doesn't get reversed on page refresh.
         @random = SecureRandom.uuid
