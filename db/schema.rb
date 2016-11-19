9e15301a92429863d1bab40253adb4afa6a4c792
Andrea <arhallier@gmail.com>
2016-11-19 19:15:20 +0000

    groups page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103151014) do
+ActiveRecord::Schema.define(version: 20161103151016) do
 
   create_table "cards", force: :cascade do |t|
     t.integer  "deck_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20161103151014) do
     t.boolean  "public"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "decks_groups", id: false, force: :cascade do |t|
@@ -39,14 +40,6 @@ ActiveRecord::Schema.define(version: 20161103151014) do
 
   add_index "decks_groups", ["deck_id"], name: "index_decks_groups_on_deck_id"
   add_index "decks_groups", ["group_id"], name: "index_decks_groups_on_group_id"
-
-  create_table "decks_users", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "deck_id"
-  end
-
-  add_index "decks_users", ["deck_id"], name: "index_decks_users_on_deck_id"
-  add_index "decks_users", ["user_id"], name: "index_decks_users_on_user_id"
 
   create_table "groups", force: :cascade do |t|
     t.text     "title"
@@ -60,6 +53,14 @@ ActiveRecord::Schema.define(version: 20161103151014) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users_fav_decks", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "deck_id"
+  end
+
+  add_index "users_fav_decks", ["deck_id"], name: "index_users_fav_decks_on_deck_id"
+  add_index "users_fav_decks", ["user_id"], name: "index_users_fav_decks_on_user_id"
 
   create_table "users_groups", id: false, force: :cascade do |t|
     t.integer "users_id"
