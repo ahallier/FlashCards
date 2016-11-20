185b20528de378c1d7706df8a92675e1f86ce873
Sydney Garcia <sydney-garcia@uiowa.edu>
2016-11-20 20:55:25 +0000

    various minor changes

---
+++ spec/models/deck_spec.rb
@@ -0,0 +1,29 @@
+describe Deck do
+    describe 'searching Deck by keyword' do
+        context 'with keyword' do
+            it 'should search Deck with keyword' do
+                deck = Deck.create
+                deck.title = "test"
+                Deck.search('test')
+            end
+        end
+    end
+end
+
+
+#describe Movie do
+#  describe 'searching Tmdb by keyword' do
+#    context 'with valid key' do
+#      it 'should call Tmdb with title keywords' do
+#        expect( Tmdb::Movie).to receive(:find).with('Inception')
+#        Movie.find_in_tmdb('Inception')
+#      end
+#    end
+#    context 'with invalid key' do
+#      it 'should raise InvalidKeyError if key is missing or invalid' do
+#        allow(Tmdb::Movie).to receive(:find).and_raise(Tmdb::InvalidApiKeyError)
+#        expect {Movie.find_in_tmdb('Inception') }.to raise_error(Movie::InvalidKeyError)
+#      end
+#    end
+#  end
+#end
