dacb1803afa20f40196c63589a014bf6ef66bc84
Andrea <arhallier@gmail.com>
2016-11-20 02:52:45 +0000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161116022537) do
 
   create_table "cards", force: :cascade do |t|
@@ -52,7 +51,6 @@ ActiveRecord::Schema.define(version: 20161116022537) do
   create_table "users", force: :cascade do |t|
     t.text     "email"
     t.text     "password"
-    t.string   "session_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
