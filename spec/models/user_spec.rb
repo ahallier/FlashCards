29d9c7a9a270a26cb3250500f79af9dba2174fb0
Ryan Kuemmel <ryan-kuemmel@uiowa.edu>
2016-11-21 05:34:03 +0000

    fixed user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
-describeUser do
+describe User do
     describe 'Creating a user' do
         context 'with email and password' do
             it 'should should create a user' do
+                user = User.create()
                 user.email = "ryan@email.com"
                 user.password = "password"
-                User.create()
-            end
+                end
         end
     end
 end
