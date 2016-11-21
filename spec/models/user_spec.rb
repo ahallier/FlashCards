cd7fd75452bb31a091e761ad3115eed59eaf9d71
Andrea <arhallier@gmail.com>
2016-11-21 05:35:02 +0000

    trying user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
-describeUser do
+describe User do
     describe 'Creating a user' do
         context 'with email and password' do
             it 'should should create a user' do
-                deck = Deck.create
+                user = User.create
                 user.email = "ryan@email.com"
                 user.password = "password"
-                User.create()
+                User.create_user!(user)
             end
         end
     end
