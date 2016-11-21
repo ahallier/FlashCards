cf27e75f2e48f8ecc525e99aca77ce9cc688347e
Sydney Garcia <sydney-garcia@uiowa.edu>
2016-11-21 05:52:37 +0000

    heroku url

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 describe User do
     describe 'Creating a user' do
         context 'with email and password' do
-            it 'should should create a user' do
+            it 'should create a user' do
                 user = User.create()
                 user.email = "ryan@email.com"
                 user.password = "password"
-                end
+            end
         end
     end
 end
