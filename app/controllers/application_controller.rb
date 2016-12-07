5b3dd3363ac295b565787020301e7d00f9e4ece3
Andrea <arhallier@gmail.com>
2016-12-07 18:42:34 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def set_current_user
     @current_user ||=	session[:session_token	] && User.find_by_session_token(session[:session_token])
-  end
+end
 end
